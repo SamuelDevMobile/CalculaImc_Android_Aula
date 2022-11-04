74236ca0f7adca4251e524a0963862a0b4dfe722
Samuel Ribeiro <samuelribeiro9@icloud.com>
2022-11-04 01:18:55 -0300

    adicionado mais um teste unitario

--- CalculadoraIMC/app/src/test/java/com/example/calculadoraimc/UtilTest.kt
+++ CalculadoraIMC/app/src/test/java/com/example/calculadoraimc/UtilTest.kt
@@ -6,17 +6,23 @@ import org.junit.Test
 class UtilTest {
 
     @Test
-    fun testGetImcResult_90_180_sobrepeso() {
+    fun `testGetImcResult_90_180_sobrepeso`() {
         val result = getImcResult("90", "1.80")
         assertEquals(result.result, Result.SOBREPESO)
     }
 
     @Test
-    fun testGetImcResult_aviso_embranco() {
+    fun `testGetImcResult_aviso_embranco`() {
         val semPeso = getImcResult("", "1.76")
         assertEquals(semPeso.result, Result.BLANK)
 
         val semAltura = getImcResult("67", "")
         assertEquals(semAltura.result, Result.BLANK)
+    }
+
+    @Test
+    fun `testaGetImcResult_67_176_saudavel`() {
+        val imcSaudavel = getImcResult("67", "1.76")
+        assertEquals(imcSaudavel.result, Result.OK)
     }
 }
